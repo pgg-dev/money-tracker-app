6cd3ca89f710446baf1a00afad5b422eab9eebfa
pgg-dev <park.geumgyu@gmail.com>
2025-12-19 13:57:56 +0900

    chore: react query devtools 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@/shared/theme"; // 내가 정한 디자인 규칙
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
